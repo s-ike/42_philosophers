dedd4e5d592ec645beb9bc15499c1fbaf52f95c1
s-ike <shunt_i@yahoo.co.jp>
2021-10-12 15:08:20 +0900

    update print function

--- philo/srcs/ft_mutex_print.c
+++ philo/srcs/ft_mutex_print.c
@@ -6,30 +6,36 @@
 /*   By: sikeda <sikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:19:20 by sikeda            #+#    #+#             */
-/*   Updated: 2021/10/12 12:40:33 by sikeda           ###   ########.fr       */
+/*   Updated: 2021/10/12 15:07:34 by sikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mutex_print.h"
 
+static char
+	*get_msg(t_philo_status status)
+{
+	if (status == ST_DIE)
+		return (COLOR_B_RED"is died"COLOR_RESET);
+	else if (status == ST_FORK)
+		return (COLOR_CYAN"has taken a fork"COLOR_RESET);
+	else if (status == ST_EAT)
+		return (COLOR_GREEN"is eating"COLOR_RESET);
+	else if (status == ST_SLEEP)
+		return (COLOR_BLUE"is sleeping"COLOR_RESET);
+	else if (status == ST_THINK)
+		return (COLOR_YELLOW"is thinking"COLOR_RESET);
+	return (NULL);
+}
+
 static void
 	print(t_philo *philo, t_philo_status status, t_time time)
 {
 	static t_time	start = 0;
-	char			*msg;
 
-	msg = COLOR_B_RED"is died"COLOR_RESET;
-	if (status == ST_FORK)
-		msg = COLOR_CYAN"has taken a fork"COLOR_RESET;
-	else if (status == ST_EAT)
-		msg = COLOR_GREEN"is eating"COLOR_RESET;
-	else if (status == ST_SLEEP)
-		msg = COLOR_BLUE"is sleeping"COLOR_RESET;
-	else if (status == ST_THINK)
-		msg = COLOR_YELLOW"is thinking"COLOR_RESET;
 	if (start == 0)
 		start = ft_get_mstime();
-	printf("%llu %d %s\n", time - start, philo->id, msg);
+	printf("%llu %d %s\n", time - start, philo->id, get_msg(status));
 }
 
 void

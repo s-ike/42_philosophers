2debf912da87c43d6bb3df6a11d245380679dee5
s-ike <shunt_i@yahoo.co.jp>
2021-10-12 17:09:43 +0900

    fix crash when there are negative numbers in the args

--- philo/srcs/ft_utils/ft_isnumeric.c
+++ philo/srcs/ft_utils/ft_isnumeric.c
@@ -6,7 +6,7 @@
 /*   By: sikeda <sikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 09:58:01 by sikeda            #+#    #+#             */
-/*   Updated: 2021/10/12 12:43:23 by sikeda           ###   ########.fr       */
+/*   Updated: 2021/10/12 17:07:55 by sikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int
 {
 	if (!str || *str == '\0')
 		return (0);
-	if (*str == '+' || *str == '-')
-		str++;
 	if (!ft_isdigit(*str))
 		return (0);
 	while (*str)
